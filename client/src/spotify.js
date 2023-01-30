30f5743a48c1637aa09058c51b4ebbbd6a5505c3
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-01-30 14:16:15 -0600

    update the way we retrieve the Spotify access token

--- client/src/spotify.js
+++ client/src/spotify.js
@@ -21,15 +21,51 @@ const LOCALSTORAGE_VALUES = {
 // Once we're done modifying our App.js file,  Now, although we're able to let users log in to Spotify and grab the access token from the URL, we still have a problem. Our React app is only aware of the Spotify access token when it's stored as a query parameter on the URL — meaning if we visit http://localhost:3000 again, we'll see the login button, not the logged-in state
 // To fix this, we can store our tokens in local storage, a mechanism of the Web Storage API which lets us store key/value pairs in the browser.  Local storage will let us store data in the browser for a particular domain and persist it, even when the user closes the tab or navigates away from our app => Step 8 (index.js)
 // Step 9: update the getAccessToken() function to store tokens in local storage the first time a user logs in, as well as to pull the tokens from local storage the next time they are available.
+/**
+ * Handles logic for retrieving the Spotify access token from localStorage
+ * or URL query params
+ * @returns {string} A Spotify access token
+ */
 const getAccessToken = () => {
+    // All of our query params logic
     // window.location.search returns the values of everthing after the http://localhost:3000/ on the URL after Step 4 (http://localhost:3000/?access_token=BQBtyTL32CXr.....)
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     // get the access token and refresh token from urlParams.  Now we're ready to use those tokens to make API calls to the Spotify API -> Step 6
-    const accessToken = urlParams.get('access_token');
-    const refreshToken = urlParams.get('refresh_token');
+    // const accessToken = urlParams.get('access_token');
+    // const refreshToken = urlParams.get('refresh_token');
+    
+    //using our LOCALSTORAGE_KEYS as a way to accurately grab the values from local storage
+    const queryParams = {
+        [LOCALSTORAGE_KEYS.accessToken]: urlParams.get('access_token'),
+        [LOCALSTORAGE_KEYS.refreshToken]: urlParams.get('refresh_token'),
+        [LOCALSTORAGE_KEYS.expireTime]: urlParams.get('expires_in'),
+    };
 
-    return accessToken;
+    // If there's an error in the query param of our URL OR the token in localStorage has expired, refresh the token
+    if (hasError || hasTokenExpired() || LOCALSTORAGE_VALUES.accessToken === 'undefined') {
+        refreshToken();
+    }
+
+    // If there is a valid access token in localStorage, use that
+    if (LOCALSTORAGE_VALUES.accessToken && LOCALSTORAGE_VALUES.accessToken !== 'undefined') {
+        return LOCALSTORAGE_VALUES.accessToken;
+    }
+
+    // If there is NO access token in local storage and there is a token in the URL query params, user is logging in for the first time
+    if (queryParams[LOCALSTORAGE_KEYS.accessToken]) {
+        // Store the query params in localStorage
+        for (const property in queryParams) {
+            window.localStorage.setItem(property, queryParams[property]);
+        }
+        // Set timestamp
+        window.localStorage.setItem(LOCALSTORAGE_KEYS.timestamp, Date.now());
+        // Return access token from query params
+        return queryParams[LOCALSTORAGE_KEYS.accessToken];
+    }
+
+    // We should never get here!
+    return false;
 };
 
 export const accessToken = getAccessToken();
