6c8616fa29a6fae3300ed64d40e73c7e517572ad
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-01-31 00:42:54 -0600

    get and display public playlists

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { catchErrors } from '../utils';
 import { getCurrentUserProfile, getCurrentUserPlaylists, getTopArtists, getTopTracks } from '../spotify';
 import { StyledHeader } from '../styles';
-import { SectionWrapper, ArtistsGrid, TrackList } from '../components';
+import { SectionWrapper, ArtistsGrid, TrackList, PlaylistsGrid } from '../components';
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
@@ -53,7 +53,7 @@ const Profile = () => {
           </StyledHeader>
       )}
 
-      {topArtists && topTracks && (
+      {topArtists && topTracks && playlists && (
             <main>
               <SectionWrapper title="Top artists this month" seeAllLink="/top-artists">
                 <ArtistsGrid artists={topArtists.items.slice(0, 10)} />
@@ -62,8 +62,12 @@ const Profile = () => {
               <SectionWrapper title="Top tracks this month" seeAllLink="/top-tracks">
                 <TrackList tracks={topTracks.items.slice(0, 10)} />
               </SectionWrapper>
+
+              <SectionWrapper title="Playlists" seeAllLink="/playlists">
+                <PlaylistsGrid playlists={playlists.items.slice(0, 10)} />
+              </SectionWrapper>
             </main>
-      )}
+        )}
     </>
   )
 };
